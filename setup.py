aae5c6d8f71388741a3fcf6870ddfb39c504ab79
nicko170 <nick@npratley.net>
2021-03-03 15:41:31 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,11 +8,7 @@ except ImportError: # for pip <= 9.0.3
     from pip.req import parse_requirements
 import uuid
 
-# parse_requirements() returns generator of pip.req.InstallRequirement objects
 install_reqs = parse_requirements('requirements.txt', session=uuid.uuid1())
-
-# reqs is a list of requirement
-# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
 reqs = [str(ir.req) for ir in install_reqs]
 
 version = '0.2'
